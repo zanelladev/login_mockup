668eb1c5b615a99daac393bb45040815919353ac
Artur Zanella <arturf.zanella@gmail.com>
2023-06-16 00:54:45 -0300

    some adds

---
+++ lib/app/constants/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+ThemeData defaultTheme = ThemeData(
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(
+      color: Colors.white,
+      fontFamily: "Roboto",
+      fontSize: 40,
+      fontWeight: FontWeight.bold,
+    ),
+    displayMedium: TextStyle(
+      color: Color.fromARGB(255, 130, 130, 134),
+      fontFamily: "Roboto",
+      fontSize: 20,
+      fontWeight: FontWeight.normal,
+    ),
+  ),
+  colorScheme: const ColorScheme(
+    background: Colors.black,
+    brightness: Brightness.dark,
+    error: Colors.red,
+    onBackground: Colors.white,
+    onError: Colors.white,
+    onPrimary: Colors.black,
+    onSecondary: Colors.black,
+    onSurface: Colors.black,
+    primary: Colors.black,
+    secondary: Colors.black,
+    surface: Colors.black,
+  ),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
+import 'app/constants/app_theme.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: defaultTheme,
       home: const LoginPage(),
     );
   }
@@ -23,11 +18,66 @@ class MyApp extends StatelessWidget {
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    MediaQueryData deviceInfo = MediaQuery.of(context);
     return Scaffold(
-      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: deviceInfo.size.width,
+        height: deviceInfo.size.height,
+        child: Container(
+          decoration: BoxDecoration(
+            color: theme.colorScheme.background,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: deviceInfo.size.width * 0.75,
+                child: Text(
+                  "Get your Money Under Control",
+                  style: theme.textTheme.titleLarge,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                width: deviceInfo.size.width * 0.75,
+                child: Text(
+                  "Manage your Expenses. Seamlessly.",
+                  style: theme.textTheme.displayMedium,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color.fromARGB(255, 94, 92, 229),
+                  fixedSize: Size(
+                    deviceInfo.size.width * 0.9,
+                    deviceInfo.size.height * 0.07,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text('Sign Up with Email ID'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.grey,
+                  backgroundColor: Colors.white,
+                  fixedSize: Size(
+                    deviceInfo.size.width * 0.9,
+                    deviceInfo.size.height * 0.07,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text('Sign Up with Google'),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
